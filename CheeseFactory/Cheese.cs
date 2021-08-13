15dbf763658125a8cb04d5eb57e35c4e830917e6
BuildTools <unconfigured@null.spigotmc.org>
2021-08-13 14:44:46 +0200

    Add project files.

---
+++ CheeseFactory/Cheese.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace CheeseFactory
+{
+    public class Cheese
+    {
+        int ID, BatchNum;
+        string strName;
+        DateTime? mDate, bbDate;
+
+        public Cheese()
+        {
+            ID = 0;
+            BatchNum = 0;
+            strName = "";
+            mDate = null;
+            bbDate = null;
+        }
+
+        public Cheese(int id, int batchnumber, string name, DateTime? MDate, DateTime? BBDate)
+        {
+            ID = id;
+            BatchNum = batchnumber;
+            strName = name;
+            mDate = MDate;
+            bbDate = BBDate;
+        }
+
+        public string CheeseToString()
+        {
+            return "Cheese Name: " + strName +
+                "\nCheese ID: " + ID +
+                "\nCheese Batch Number: " + BatchNum +
+                "\nManufacture Date: " + mDate +
+                "\nBest Before Date: " + bbDate;
+        }
+
+        public object this[string attrName]
+        {
+            get
+            {
+                if (attrName.ToLower().Equals("id"))
+                {
+                    return this.ID;
+                }
+                else if (attrName.ToLower().Equals("batchnum"))
+                {
+                    return this.BatchNum;
+                }
+                else if (attrName.ToLower().Equals("name"))
+                {
+                    return this.strName;
+                }
+                else if (attrName.ToLower().Equals("mdate"))
+                {
+                    return this.mDate;
+                }
+                else if (attrName.ToLower().Equals("bbdate"))
+                {
+                    return this.bbDate;
+                }
+
+                return null;
+            }
+
+            set
+            {
+                if (attrName.ToLower().Equals("id"))
+                {
+                    this.ID = (int)value;
+                }
+                else if (attrName.ToLower().Equals("batchnum"))
+                {
+                    this.BatchNum = (int)value;
+                }
+                else if (attrName.ToLower().Equals("name"))
+                {
+                    this.strName = (string)value;
+                }
+                else if (attrName.ToLower().Equals("mdate"))
+                {
+                    this.mDate = (DateTime?)value;
+                }
+                else if (attrName.ToLower().Equals("bbdate"))
+                {
+                    this.bbDate = (DateTime?)value;
+                }
+            }
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return this.ID;
+                    case 1:
+                        return this.BatchNum;
+                    case 2:
+                        return this.strName;
+                    case 3:
+                        return this.mDate;
+                    case 4:
+                        return this.bbDate;
+                    default:
+                        break;
+                }
+                return null;
+            }
+
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        this.ID = (int)value;
+                        break;
+                    case 1:
+                        this.BatchNum = (int)value; ;
+                        break;
+                    case 2:
+                        this.strName = (string)value;
+                        break;
+                    case 3:
+                        this.mDate = (DateTime?)value;
+                        break;
+                    case 4:
+                        this.bbDate = (DateTime?)value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ CheeseFactory/CheeseProduction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheeseFactory
+{
+    public class CheeseProduction
+    {
+        List<Cheese> Factory = new List<Cheese>();
+
+        private int iSize = 0;
+        public int ListSize { get => iSize; }
+
+        public void addCheese(int id, int batchnumber, string name, DateTime? MDate, DateTime? BBDate)
+        {
+            Factory.Add(new Cheese(id, batchnumber, name, MDate, BBDate));
+            iSize++;
+        }
+
+        public void removeCheese(Cheese toRemove)
+        {
+            Factory.Remove(toRemove);
+        }
+
+        public List<string> CheeseList()
+        {
+            List<string> output = new List<string>();
+
+            for (int i = 0; i < ListSize; i++)
+            {
+                output.Add(Factory[i].CheeseToString());
+            }
+
+            return output;
+        }
+
+
+        public Cheese search(int ID)
+        {
+            Cheese toReturn = null;
+
+            for (int i = 0; i < ListSize; i++)
+            {
+                Cheese temp = Factory[i];
+                if ((int)temp["id"] == ID)
+                {
+                    toReturn = temp;
+                }
+            }
+
+            return toReturn;
+        }
+    }
+}
